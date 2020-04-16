ddfafd7692acf196b3e70c9a37f5b2544c9c11b2
assad <assadullah.ghaznawi@gmail.com>
2020-04-16 21:57:21 +0200

    version 9

--- ReadFilesCases/EasyJsonReader.cs
+++ ReadFilesCases/EasyJsonReader.cs
@@ -6,13 +6,19 @@ using System.Threading.Tasks;
 
 namespace ReadFilesCases
 {
-    class EasyJsonReader : ReadFileHandler
+    class EasyJsonReader : ReadFileHandler, EasyFileReader
     {
         public FileType FileType { get; set; }
-
+        public Boolean IsRoleBased { get; set; }
+        public Role UserRole { get; set; }
         public EasyJsonReader(string path) : base(path)
         {
             FileType = FileType.Json;
+        }
+        public EasyJsonReader(string path, Boolean isRoleBased, Role userRole) : this(path)
+        {
+            IsRoleBased = isRoleBased;
+            UserRole = userRole;
         }
         public EasyJsonReader(string path, Boolean IsEncryptionActive) : base(path, IsEncryptionActive)
         {
@@ -20,6 +26,10 @@ namespace ReadFilesCases
         }
         public new string ReadFile()
         {
+            if (IsRoleBased)
+            {
+                return base.ReadFileBySecurityrRole(UserRole);
+            }
             return base.ReadFile();
         }
     }
