d8c20e147fde13bc9d6645fb5ee8344107ac233d
assad <assadullah.ghaznawi@gmail.com>
2020-04-16 20:13:11 +0200

    version 1

---
+++ ReadFilesCases/EasyTextReader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadFilesCases
+{
+    class EasyTextReader : ReadFileHandler
+    {
+        public FileType FileType { get; set; }
+        public EasyTextReader(string path) : base(path)
+        {
+            FileType = FileType.Txt;
+        }
+
+        public new string ReadFile()
+        {
+            return base.ReadFile();
+        }
+    }
+}
---
+++ ReadFilesCases/ReadFileHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ReadFilesCases
+{
+    public enum FileType
+    {
+        Txt
+    }
+    public class ReadFileHandler
+    {
+        protected string Path { get; set; }
+        public ReadFileHandler(string path)
+        {
+            Path = path;
+        }
+        protected string ReadFile()
+        {
+            try
+            {
+                StreamReader streamReader = new StreamReader(Path);
+                string result = streamReader.ReadToEnd();
+                streamReader.Close();
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                return ("Error " + e.Message);
+            }
+        }
+    }
+}
